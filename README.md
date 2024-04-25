15c0af8a9b19bb4c3a7fe49b3f0f64dd22f1d078
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-04-25 17:34:34 -0300

    Projeto feito

---
+++ README.md
@@ -0,0 +1,11 @@
+# Classificador de Nível de Herói
+
+Projeto feito para o desafio "Classificador de Nível de Herói"
+
+## Requisitos
+
+- Node.js
+
+## Como usar
+
+- Utilize ```node index.js``` num prompt de comando para executar
---
+++ index.js
@@ -0,0 +1,23 @@
+const heroiNome = "DIO";
+const xp = 9100; 
+let nivel;
+
+if(xp < 1001){
+    nivel = "Ferro";
+}else if(xp > 1000 && xp < 2001){
+    nivel = "Bronze";
+}else if(xp > 2000 && xp < 5001){
+    nivel = "Prata";
+}else if(xp > 5000 && xp < 7001){
+    nivel = "Ouro";
+}else if(xp > 7000 && xp < 8001){
+    nivel = "Platina";
+}else if(xp > 8000 && xp < 9001){
+    nivel = "Ascendente";
+}else if(xp > 9000 && xp < 10001){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome **${heroiNome}** está no nível de **${nivel}**`);
